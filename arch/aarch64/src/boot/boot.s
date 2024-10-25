31c135412abc2482fb6b431bd21de193e2a8c6aa
Tom Anderson <tom@ando.gq>
2024-10-26 10:31:25 +1100

    zero out `bss` during boot

--- arch/aarch64/src/boot/boot.s
+++ arch/aarch64/src/boot/boot.s
@@ -1,3 +1,12 @@
+// Load the relative address (to the PC) of a value.
+// The symbol must be within +/- 4Gb of the PC.
+.macro ADR_REL register, symbol
+    // An offset to the page into the upper part of the register
+    adrp    \register, \symbol
+    // Load the lower bits of the symbol address, which are the offset into the page.
+    add     \register, \register, #:lo12:\symbol
+.endm
+
 1:
     // Core check: only boot if in EL2
     mrs     x0,     CurrentEL
@@ -10,7 +19,20 @@
     cmp     x1,     {CONST_BOOT_CORE_ID}
     b.ne    9f      // v  Loop
 
-2:
+    // Initialise BSS
+    ADR_REL x0,     __bss_start
+    ADR_REL x1,     __bss_end_exclusive
+
+1:
+    // If the pointers meet each other, then BSS has been cleared.
+    cmp     x0,     x1
+    b.eq   2f
+
+    // Zero out the current pointer location, and increment the pointer by 16 to the next position.
+    stp     xzr, xzr, [x0], #16
+    b 1b
+
+2: // Success loop
     nop
     wfe
     nop
